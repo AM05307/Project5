da535fcbd13ebdd17a665daaf66ef0a9137e10ac
AM05307 <eth05307@gmail.com>
2018-05-06 23:32:53 +0900

    0

--- Project5/gittest.c
+++ Project5/gittest.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 int main() {
-	int a = 10;
-	printf("%d\n", a);
+	printf("1) 10 / 3 = %d \n", 10 / 3);
+	printf("2) 10 / 3 = %f \n", 10 / 3);
+	printf("3) 10. / 3. = %f \n", 10. / 3.);
+	printf("4) 10. / 3. = %d \n", 10. / 3.);
 }
