e5816723ab4975484472bc8dbd1bcb05b0042ac1
AM05307 <eth05307@gmail.com>
2018-05-08 13:42:23 +0900

    ch 8-12

---
+++ Project5/ch8-12.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+	int a = 10, *p, **pp;
+	p = &a;
+	pp = &p;
+	printf("%d  %d  %d\n ", a, *p, **pp);
+	printf("self address :    %p  %p %p\n ", &a, &p, &pp);
+	printf("pointer address :  %p  %p %p\n ", a, p, pp);
+	return  0;
+}
