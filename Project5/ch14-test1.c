9c97d433f53b368f177a408fd58f458ceb2f4e4a
AM05307 <eth05307@gmail.com>
2018-05-10 16:09:05 +0900

    ch14-test1js.c [과제제시 - 과제포함]
    조건과 같이 파일의  Input.txt 파일의 내용을 읽어서 총점 평균을 구한 Output.txt 파일에
    파일에 내용과 같이  저장한다. fopen_s()와 fprintf_s(),fscanf_s()를 이용 합니다
    
    [Input.txt]
    라바    90 76 78
    타요 100 100 100
    뽀로로  76 97 58
    [Output.txt]
    이름     총점    평균
    ---------------------
    라바   244     81.33
    타요    300    100.00
    뽀로로   231     77.00

--- Project5/ch14-test1.c
+++ Project5/ch14-test1.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
 
 void main() {
-	FILE *f; //파일의 구조체 변수
+	FILE *inputfile; //input.txt파일의 구조체 변수
 	char c;
-	fopen_s(&f, "input.txt", "r");  //파일을 오픈하여 읽기모드 지정
-	while ((c= fread((f) != EOF)){
-		putchar(c);
+	fopen_s(&inputfile, "input.txt", "r");  //파일을 오픈하여 읽기모드 지정
+	if (inputfile == NULL) {
+		printf("파일이 없습니다.\n");
 	}
+	else {
+		printf("파일을 읽었습니다.\n");
+	}
+
+	fscanf_s(inputfile,"%s %d\n",name,al)
+
+
 };
